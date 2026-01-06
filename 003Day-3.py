e6174dde7df75632124be372d3e5c435d15f59c9
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-01-06 22:24:59 +0000

    Add files via upload

---
+++ 003Day-3.py
@@ -0,0 +1,14 @@
+#Modules and Pip!!
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+import scipy
+import statsmodels.api as sm
+import tensorflow as tf
+import torch
+import keras
+import xgboost
+import lightgbm
+import catboost
+Print("All libraries imported successfully!")
