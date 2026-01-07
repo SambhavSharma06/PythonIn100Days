63c22354288d89a7d0d47921a77c7503fe7d4c8b
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-01-07 19:10:33 +0000

    Add files via upload

---
+++ 006Day-6.py
@@ -0,0 +1,20 @@
+#Variables and Data Types!!
+a="Sambhav Sharma"#String
+b=7#Integer
+c=8.9#Float
+d=True#Boolean
+e=None#NoneType
+f=[1,2,3,4,5]#List
+g=(1,2,3,4,5)#Tuple
+h={1,2,3,4,5}#Set
+i={"name":"Sambhav","age":20,"city":"New Delhi"}#Dictionary
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
