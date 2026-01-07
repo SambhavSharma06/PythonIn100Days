68a78c095371998233da9926165d81dd5426a4bd
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-01-07 02:12:22 +0000

    Add files via upload

---
+++ 005Day-5.py
@@ -0,0 +1,13 @@
+#Comments, Escape Sequences & Print Statement!!
+# Don't gve up please!!
+print("Day 5 completed successfully!\n"
+      "see you tomorrow")#Print bro
+'''
+print("Hello, World!")
+print(2+6)
+print("End Day 4")
+'''
+
+print("Let's \"start Day\" 5")
+print("Let's \'start Day\' 5")
+print("Sam",7,8,sep="_",end="**")
