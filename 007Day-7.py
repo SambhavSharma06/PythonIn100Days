1befc3a60f9bd69843e36c730268512a4a297b45
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-01-08 19:27:09 +0000

    Add files via upload

---
+++ 007Day-7.py
@@ -0,0 +1,36 @@
+#Exercise 1: Calculator using Python!!
+#CALCULATOR
+print("Welcome to the Calculator Program")
+print("Select operation:"
+      "\n1.Addition For adittion type +"
+      "\n2.Subtraction For subtraction type -"
+      "\n3.Multiplication For multiplication type *"
+      "\n4.Division For division type /"
+      "\n5.Exit For exit type EXIT")
+print(input("Enter operation: "))
+
+if(input=='+'):
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    result = num1 + num2
+    print("The result is: ", result)
+elif(input=='-'):
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    result = num1 - num2
+    print("The result is: ", result)
+elif(input=='*'):
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    result = num1 * num2
+    print("The result is: ", result)
+elif(input=='/'):
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    result = num1 / num2
+    print("The result is: ", result)
+elif(input=='EXIT'):
+    print("Exiting the Calculator Program. Goodbye!")
+    print(exit)
+else:
+    print("Invalid input")
