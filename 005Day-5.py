d9f88c2e49a85d8f3960c3d0c9d88fffb00e5a2e
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-01-12 00:19:27 +0000

    Fix print statements and comments formatting

--- 005Day-5.py
+++ 005Day-5.py
@@ -1,7 +1,7 @@
 #Comments, Escape Sequences & Print Statement!!
 # Don't gve up please!!
 print("Day 5 completed successfully!\n"
-      "see you tomorrow")#Print bro
+      "see you tomorrow")
 '''
 print("Hello, World!")
 print(2+6)
@@ -10,4 +10,5 @@ print("End Day 4")
 
 print("Let's \"start Day\" 5")
 print("Let's \'start Day\' 5")
+
 print("Sam",7,8,sep="_",end="**")
