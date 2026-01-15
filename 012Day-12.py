30a7d6ccaf4e5d8982d199ee3c841c949df9df45
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-01-15 20:03:50 +0000

    Add files via upload

---
+++ 012Day-12.py
@@ -0,0 +1,9 @@
+#Strings Slicing and Operations on Strings in Python!!
+name="SambhavSharma"
+length=len(name)
+
+print("Length of the name is:",length)
+
+print((name[0:2]))
+
+print((name[-5:-1]))
