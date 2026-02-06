e4400b8f67059ad3a432840be2c3ca0bd761608c
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-02-06 01:56:41 +0000

    Add files via upload

---
+++ 024Day-24.py
@@ -0,0 +1,20 @@
+#Tuples in Python!!
+
+tup = (1,2,3,4,5,"Hello",True)
+print(tup)
+
+print(tup[0])
+
+print(len(tup))
+
+print(tup[-1])
+
+print(tup[1:5])
+
+print(tup[2])
+
+if 3 in tup:
+    print("3 is present in the tuple")
+tup2 = (6,7,8)
+
+print(tup2)
