6554594cf9086e03353114992c144fc103b9800d
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-02-12 22:02:20 +0000

    Add files via upload

---
+++ 031Day-31.py
@@ -0,0 +1,13 @@
+#Sets in Python!!
+
+s ={2,4,2,6}#No repetation
+print(s)
+
+info = {"Sam",19,False,5.9,19}
+print(info)
+
+harry = set()
+print(type(harry))
+
+for value in info:
+        print(value)
