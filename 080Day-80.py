1d4acc05991042f55aa70e0d1f5bb1f8e72eab75
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-03-24 14:48:40 +0000

    Add files via upload

---
+++ 080Day-80.py
@@ -0,0 +1,22 @@
+# Multilevel Inheritance in Python!!
+
+# Base class
+class Grandfather:
+    def house(self):
+        print("Grandfather has a house")
+
+# Inherits from Grandfather
+class Father(Grandfather):
+    def car(self):
+        print("Father has a car")
+
+# Inherits from Father (and indirectly Grandfather)
+class Son(Father):
+    def bike(self):
+        print("Son has a bike")
+
+# Create object of Son
+obj = Father()
+
+obj.house()   # From Grandfather
+obj.car()     # From Father
