35ba7f90cb23b7fc11599ba4474662f3d01553bd
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-03-24 14:50:01 +0000

    Add files via upload

---
+++ 082Day-82.py
@@ -0,0 +1,23 @@
+# Merge the Pdf Solution in Python!!
+
+from PyPDF2 import PdfMerger
+
+# Create a PdfMerger object
+merger = PdfMerger()
+
+# List of PDF files in order
+pdf_files = [
+    "1.CV.pdf",
+    "2.Customer Invoice (Paid).pdf",
+    "3.Customer Invoice.pdf"
+]
+
+# Append each PDF
+for pdf in pdf_files:
+    merger.append(pdf)
+
+# Write merged output
+merger.write("merged_output.pdf")
+merger.close()
+
+print("PDFs merged successfully into 'merged_output.pdf'")
