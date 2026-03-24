87aa21965e39ae30ed9439fb16aa18d674b24ae5
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-03-24 14:52:18 +0000

    Add files via upload

---
+++ 085Day-85.py
@@ -0,0 +1,12 @@
+# Creating command line utility in python!!
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Example CLI tool")
+
+parser.add_argument("--name", help="Your name")
+parser.add_argument("--age", type=int, help="Your age")
+
+args = parser.parse_args()
+
+print(f"Hello {args.name}, you are {args.age} years old.")
