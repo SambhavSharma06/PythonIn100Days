d7f19ed5bf6d42edf048e05d054333607a187480
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-03-24 14:55:30 +0000

    Add files via upload

---
+++ 091Day-91.py
@@ -0,0 +1,13 @@
+# Generators in Python!!
+
+def gen():
+    for i in range(1, 10):
+        if i % 2 == 0:
+            yield i
+
+g = gen()
+
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
