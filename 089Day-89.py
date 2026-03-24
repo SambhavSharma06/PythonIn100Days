d78d16c0a33689d0d84ad30a0d47562b172179a8
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-03-24 14:54:54 +0000

    Add files via upload

---
+++ 089Day-89.py
@@ -0,0 +1,21 @@
+# Requests Module in Python!!
+
+import requests
+import time
+
+while True:
+    url = "https://api.coingecko.com/api/v3/simple/price"
+
+    params = {
+        "ids": "bitcoin",
+        "vs_currencies": "usd"
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    price = data["bitcoin"]["usd"]
+
+    print(f"Bitcoin Price: ${price}")
+
+    time.sleep(5)
