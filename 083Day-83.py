5454893c3b833fd1a02bebb6faee4a25cc5cdee1
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-03-24 14:50:28 +0000

    Add files via upload

---
+++ 083Day-83.py
@@ -0,0 +1,10 @@
+#Exercise 9 - Shoutouts to Everyone in Python!!
+
+from gtts import gTTS
+import os
+mytext = input("Enter your text: ")
+language = 'en'
+
+myobj = gTTS(text=mytext, lang=language, slow=False)
+myobj.save("welcome.mp3")
+os.system("start welcome.mp3")
