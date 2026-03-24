6b4af5f36eb17046ee52cad26985705c21b86bcf
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-03-24 14:53:42 +0000

    Add files via upload

---
+++ 087Day-87.py
@@ -0,0 +1,7 @@
+# Shutil Module in Python!!
+import shutil
+import os
+# shutil.copy("Day-67.py", "Day-67.txt")
+# shutil.copytree("Day-68.txt","Day-58.py")
+# shutil.move(".tutorial/file.file,"file.file")
+# os.remove("file.file")
