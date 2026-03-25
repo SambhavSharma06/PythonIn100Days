a0f63b55908b43f67129ad9b7fce30e74a0533df
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-03-25 12:48:18 +0000

    Add files via upload

---
+++ 092Day-92.py
@@ -0,0 +1,12 @@
+# Function Caching in Python!!
+
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n-1) + fib(n-2)
+
+
+print(fib(10))
