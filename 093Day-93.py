7bece28f983fb37d27d67c815d9b9ad76efc8024
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-04-20 19:24:26 +0100

    Update 093Day-93.py

--- 093Day-93.py
+++ 093Day-93.py
@@ -3,7 +3,7 @@ import requests
 url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=075a77a378a04641ad5564cad4ff8351"
 
 response = requests.get(url)
-data = response.json
+data = response.json()
 
 for article in data["articles"]:
     print(article["title"])
