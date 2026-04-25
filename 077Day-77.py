0413689c8e2092cba3b5530eebe47add943b9316
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-04-25 12:19:38 +0100

    Update 077Day-77.py

--- 077Day-77.py
+++ 077Day-77.py
@@ -1,7 +1,7 @@
 # Operator Overloading in Python!!
 class Vector:
     def __init__(self, i, j,k):
-        self.i = i
+        self. i = i
         self.j = j
         self.k = k
     def __str__(self):
