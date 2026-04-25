874c31c94d7ac3c3d8f3e0258f759f68ae19dd39
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-04-25 12:31:06 +0100

    Update 056Day-56.py

--- 056Day-56.py
+++ 056Day-56.py
@@ -3,8 +3,8 @@
 #OOPs is really important because it helps you do things much quicker and with ease.
 #Let me show you how.
 #For Example:- You made a template like this.....As a class.
-#In which you have written:- Player,Health,Level,Weapons and XP.
-#Now by using OOP you dont have to make new player class for every player you can just use this template
-#and you can have infinte players with Health,Level,Weapons and XP.
-#OOP is really important for any language and it is really important to be learn by the
+#In which you have written:- Player, Health, Level,, Weapons, and XP.
+#Now, by using OOP, you don't have to make a new player class for every player; you can use this template
+#and you can have infinite players with Health, Level, Weapons, and XP.
+#OOP is really important for any language, and it is really important to be learned by the
 #programmer to understand the real power of Python or any other language.
