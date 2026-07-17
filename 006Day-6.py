02e5e93fb7839e33ef1cf29d60c985bbf4a14df4
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-07-17 22:31:34 +0100

    Update 006Day-6.py

--- 006Day-6.py
+++ 006Day-6.py
@@ -1,14 +1,24 @@
 #Variables and Data Types!!
 a="Sambhav Sharma"#String
+a1 = "Sam"
 b=7#Integer
+b1 = 77
 c=8.9#Float
+c1 = 7.7
 d=True#Boolean
+d1 = false
 e=None#NoneType
+e1 = None
 f=[1,2,3,4,5]#List
+f1 = [6,7,8,9,0]
 g=(1,2,3,4,5)#Tuple
+g1 = (6,7,8,9,0)
 h={1,2,3,4,5}#Set
+h1 = {6,7,8,9,0}
 i={"name":"Sambhav","age":20,"city":"New Delhi"}#Dictionary
+i1 = {"id":7,"name":"Sam"}
 j = (1,2,3,4,5,6,7)# tuple
+j1 = (8,9,10,11)
 print(type(a))
 print(type(b))
 print(type(c))
