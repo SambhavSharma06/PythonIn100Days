bebb376a63878e796bf3135a7ed0636d2daa5fea
Sambhav Sharma <Sambhavsharmairl@gmail.com>
2026-08-27 14:23:45 +0100

    Update 004Day-4.py

--- 004Day-4.py
+++ 004Day-4.py
@@ -1,7 +1,8 @@
 #Our First Python Program!!
-print("Hello, World!")
-print(2+6)
-print("End Day 4")
-print("Start of day 5")
-print(2+7)
-print(2*2)
+print("Hello, World!");
+print(2+6);
+print("End Day 4");
+print("Start of day 5");
+print(2+7);
+print(2*2);
+print(2/2);
